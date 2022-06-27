0694680e5e82621aec34e299dbea47487f3470ed
gabefando <gabe.fonseca.04@gmail.com>
2022-06-26 20:03:04 -0400

    Redid everything because first version would have needed way too many if statements so I found a different way to everything basically the loop sets each individual character using one of the character index i made

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,10 @@
-// Variable equals generate button
 var generateBtn = document.querySelector("#generate");
 
-// Constant variable that holds all possiible characters
-const characters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&?";
+const upper = "QWERTYUIOPASDFGHJKLZXCVBNM";
+const lower = "qwertyuiopasdfghjklzxcvbnm";
+const number = "1234567890";
+const special = "!@#$%^&*()_+";
 
-// Write password to the #password input
 function writePassword() {
   var length2 = prompt("How long should the password be? (must be between 8-128");
   if (length2 >= 8 && length2 <= 128) {
@@ -16,85 +16,27 @@ function writePassword() {
   }
 }
 
-// 1. Series of prompts for password criteria
-// 1a. Length prompt 8-128
-// 1b. Different character types prompt
-// 2. Validate input, atleast one character type selected
-// 3. Password is generated matching selected criteria
-
 function generatePassword(length) {
   var result = ' ';
-  var passwordLength = characters.length;
   var upperCase = confirm("Do you want to include uppercase letters?");
   var lowerCase = confirm("Do you want to include lowercase letters?");
   var numerics = confirm("Do you want to include numbers?");
   var specialCase = confirm("Do you want to include special characters? (!@#$%&?)");
-  if (upperCase == true && lowerCase == true && numerics == true && specialCase == true) {
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * passwordLength));
+  
+  while (result.length < length) {
+    const collection = Math.floor(Math.random() * 4);
+
+    if (collection === 0 && upperCase) {
+      result += upper.charAt(Math.floor(Math.random() * upper.length));
+    } else if (collection === 1 && lowerCase) {
+      result += lower.charAt(Math.floor(Math.random() * lower.length));
+    } else if (collection === 2 && numerics) {
+      result += number.charAt(Math.floor(Math.random() * number.length));
+    } else if (collection === 3 && specialCase) {
+      result += special.charAt(Math.floor(Math.random() * special.length));
+    } else {
+      result += "You must pick atleast one character type!"
     }
-  } else if (upperCase == false && lowerCase == true && numerics == true && specialCase == true) {
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.replace("ABCDEFGHIJKLMNOPQRSTUVWXYZ", '').charAt(Math.floor(Math.random() * passwordLength));
-    }
-  } else if (upperCase == true && lowerCase == false && numerics == true && specialCase == true) {
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.replace("abcdefghijklmnopqrstuvwxyz", '').charAt(Math.floor(Math.random() * passwordLength));
-    }
-  } else if (upperCase == true && lowerCase == true && numerics == false && specialCase == true) {
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.replace("0123456789", '').charAt(Math.floor(Math.random() * passwordLength));
-    }
-  } else if (upperCase == true && lowerCase == true && numerics == true && specialCase == false) {
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.replace("!@#$%&?", '').charAt(Math.floor(Math.random() * passwordLength));
-    }
-  } else if (upperCase == false && lowerCase == false && numerics == true && specialCase == true) {
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.replace("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", '').charAt(Math.floor(Math.random() * passwordLength));
-    }
-  } else if (upperCase == true && lowerCase == false && numerics == false && specialCase == true) {
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.replace("abcdefghijklmnopqrstuvwxyz0123456789", '').charAt(Math.floor(Math.random() * passwordLength));
-    }
-  } else if (upperCase == true && lowerCase == true && numerics == false && specialCase == false) {
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.replace("0123456789!@#$%&?", '').charAt(Math.floor(Math.random() * passwordLength));
-    }
-  } else if (upperCase == false && lowerCase == true && numerics == false && specialCase == true) {
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.replace("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", '').charAt(Math.floor(Math.random() * passwordLength));
-    }
-  } else if (upperCase == true && lowerCase == false && numerics == true && specialCase == false) {
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.replace("", '').charAt(Math.floor(Math.random() * passwordLength));
-    }
-  } else if (upperCase == true && lowerCase == false && numerics == true && specialCase == true) {
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.replace("", '').charAt(Math.floor(Math.random() * passwordLength));
-    }
-  } else if (upperCase == false && lowerCase == true && numerics == true && specialCase == false) {
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.replace("", '').charAt(Math.floor(Math.random() * passwordLength));
-    }
-  } else if (upperCase == false && lowerCase == false && numerics == false && specialCase == true) {
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.replace("", '').charAt(Math.floor(Math.random() * passwordLength));
-    }
-  } else if (upperCase == true && lowerCase == false && numerics == false && specialCase == false) {
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.replace("", '').charAt(Math.floor(Math.random() * passwordLength));
-    }
-  } else if (upperCase == false && lowerCase == true && numerics == false && specialCase == false) {
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.replace("", '').charAt(Math.floor(Math.random() * passwordLength));
-    }
-  } else if (upperCase == false && lowerCase == false && numerics == true && specialCase == false) {
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.replace("", '').charAt(Math.floor(Math.random() * passwordLength));
-    }
-  } else {
-      result += "You must choose atleast one kind of character!";
   }
   return result;
 }
