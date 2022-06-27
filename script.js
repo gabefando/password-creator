d387c069cd474b4d0ccade6e36a0b17d2bed640d
gabefando <gabe.fonseca.04@gmail.com>
2022-06-27 08:38:53 -0400

    Reworked everything again because of bugs, fixed them

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const upper = "QWERTYUIOPASDFGHJKLZXCVBNM";
 const lower = "qwertyuiopasdfghjklzxcvbnm";
 const number = "1234567890";
 const special = "!@#$%^&*()_+";
-
+var possibleCharacters = '';
 function writePassword() {
   var length2 = prompt("How long should the password be? (must be between 8-128");
   if (length2 >= 8 && length2 <= 128) {
@@ -22,21 +22,27 @@ function generatePassword(length) {
   var lowerCase = confirm("Do you want to include lowercase letters?");
   var numerics = confirm("Do you want to include numbers?");
   var specialCase = confirm("Do you want to include special characters? (!@#$%&?)");
-  
+  if (upperCase) {
+    possibleCharacters += upper;
+  }
+  if (lowerCase) {
+    possibleCharacters += lower;
+  }
+  if (numerics) {
+    possibleCharacters += number;
+  }
+  if (specialCase) {
+    possibleCharacters += special;
+  }
+  var anything = lowerCase == false && numerics == false && specialCase == false && upperCase == false;
+  console.log(anything);
+  if (anything === true) {
+    result += "You must pick atleast one character type!";
+    return result;
+  }
   while (result.length < length) {
-    const collection = Math.floor(Math.random() * 4);
-
-    if (collection === 0 && upperCase) {
-      result += upper.charAt(Math.floor(Math.random() * upper.length));
-    } else if (collection === 1 && lowerCase) {
-      result += lower.charAt(Math.floor(Math.random() * lower.length));
-    } else if (collection === 2 && numerics) {
-      result += number.charAt(Math.floor(Math.random() * number.length));
-    } else if (collection === 3 && specialCase) {
-      result += special.charAt(Math.floor(Math.random() * special.length));
-    } else {
-      result += "You must pick atleast one character type!"
-    }
+    result += possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+   
   }
   return result;
 }
